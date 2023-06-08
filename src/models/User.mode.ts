b1cb4c50a9dcbf84151fbfceb61526902ce68d88
AndreyNedelevich <nedelevich@gmail.com>
2023-06-08 19:12:23 +0300

    lesson_4_mongoose

--- src/models/User.mode.ts
+++ src/models/User.mode.ts
@@ -8,31 +8,31 @@ import { EGenders } from "../enums/user.enum";
 const userSchema = new Schema(
   {
     name: {
-      type: String
+      type: String,
     },
     age: {
       type: Number,
       min: [1, "Minimum value for age is 1"],
-      max: [199, "Maximum value for age is 199"]
+      max: [199, "Maximum value for age is 199"],
     },
     gender: {
       type: String,
-      enum: EGenders
+      enum: EGenders,
     },
     email: {
       type: String,
       required: true,
       trim: true,
-      lowercase: true
+      lowercase: true,
     },
     password: {
       type: String,
-      required: true
-    }
+      required: true,
+    },
   },
   {
     versionKey: false,
-    timestamps: true
+    timestamps: true,
   }
   //Sхеме вторым параметром принмает конфигурационный объект.
   // versionKey: false - позволяет выключить обозначения V в объекте user оторое показывает сколько раз объект был обновлен.
