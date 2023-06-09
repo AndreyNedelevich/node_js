267f6bd7f93234ca9a5f16c892abcbed9b6478ee
AndreyNedelevich <nedelevich@gmail.com>
2023-06-09 12:04:34 +0300

    lesson_5

--- dist/repositories/user.repository.js
+++ dist/repositories/user.repository.js
@@ -4,7 +4,7 @@ exports.userRepository = void 0;
 const User_mode_1 = require("../models/User.mode");
 class UserRepository {
     async create(data) {
-        return User_mode_1.User.create(data);
+        return await User_mode_1.User.create(data);
     }
 }
 exports.userRepository = new UserRepository();
